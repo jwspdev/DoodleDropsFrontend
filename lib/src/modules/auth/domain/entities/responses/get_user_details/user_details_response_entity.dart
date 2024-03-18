6dfcfe54392014380cecdabfb23535d00e2f3953
jwspdev <63495554+jwspdev@users.noreply.github.com>
2024-03-18 16:40:11 +0800

    Auth Domain
    - included liked tags

--- lib/src/modules/auth/domain/entities/responses/get_user_details/user_details_response_entity.dart
+++ lib/src/modules/auth/domain/entities/responses/get_user_details/user_details_response_entity.dart
@@ -1,14 +1,16 @@
 import 'package:doodle_drops/src/modules/auth/domain/entities/responses/get_user_details/user_profile_response_entity.dart';
+import 'package:doodle_drops/src/modules/tags/domain/entities/tag_entity.dart';
 import 'package:equatable/equatable.dart';
 
 class UserDetailsResponseEntity extends Equatable {
   final DateTime? updatedAt;
   final String? email;
   final UserProfileResponseEntity? userProfileResponse;
+  final List<TagEntity>? likedTags;
 
   const UserDetailsResponseEntity(
-      {this.updatedAt, this.email, this.userProfileResponse});
+      {this.updatedAt, this.email, this.userProfileResponse, this.likedTags});
 
   @override
-  List<Object?> get props => [updatedAt, email, userProfileResponse];
+  List<Object?> get props => [updatedAt, email, userProfileResponse, likedTags];
 }
